a6b7e7e8deb385f3ee2459836c0affcbbdb39bb3
MartinAronov <martin.aronov@gmail.com>
2026-06-23 17:22:19 +0300

    Creating a message template for better readability in the log file.

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ timestamp = time.strftime("%Y/%m/%d %H:%M:%S")
 
 if __name__ == "__main__":
     print("Проверка на страницата за свободни места в НПМГ: " + timestamp)
+
+    msg_format = "\n--------------"  + timestamp + "--------------\n%s\n%s\n--------------------------------------------------------"
     page_diff = check()
 
     if page_diff:
         print(timestamp + " Страницата е обновена")
-        msg = "\n" + timestamp + "\n" + page_diff + "\n Провери на:" + URL
+        msg = msg_format % (page_diff, "Провери на:" + URL)
         print(msg)
         send_email("Свободни места в НПМГ", msg)
     else:
-        msg =   "\n" + time.strftime("%Y/%m/%d %H:%M:%S") + f" Няма промяна на страницата за свободни места\n"
+        msg =   msg_format % ("Няма промяна на страницата за свободни места","")
         print(msg)
     with open("output/npmg_log.log", "a", encoding="utf-8") as f:
         f.write(msg)
+
